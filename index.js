ca843ac0875bd20fb66defec7f10848e0d936060
Weston Farmer <wesfarmer11@gmail.com>
2024-01-17 10:36:03 -0600

    submitData makes post request and handles respnse, appends to DOM

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email,
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => { 
+        const userDataDiv = document.createElement('div');
+        userDataDiv.innerHTML = data.id;
+        document.body.appendChild(userDataDiv)
+    })
+
+    .catch(error => {
+        const errorMsg = document.createElement('div');
+        errorMsg.innerHTML = error.message;
+        document.body.appendChild(errorMsg);
+    })
+}
